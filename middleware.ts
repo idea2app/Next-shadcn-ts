4810fd8a60febf1030c7ab7d3744545c2fe8f18b
TechQuery <shiy2008@gmail.com>
2025-06-05 11:25:22 +0800

    [add] Dashboard demo page based on GitHub OAuth middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { patchHeaders } from "next-ssr-middleware";
 
 import { i18n } from "./i18n-config";
 
@@ -17,19 +18,16 @@ function getLocale({ headers }: NextRequest): string | undefined {
 }
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  const { pathname, search, hash } = request.nextUrl;
 
   if (pathname == "/api/og")
     return NextResponse.rewrite(request.nextUrl, {
-      headers: new Headers([
-        [
-          "cache-control",
+      headers: new Headers({
+        "cache-control":
           "max-age=3600, s-maxage=3600, stale-while-revalidate=600",
-        ],
-        ["cdn-cache-control", "max-age=3600, stale-while-revalidate=600"],
-      ]),
+        "cdn-cache-control": "max-age=3600, stale-while-revalidate=600",
+      }),
     });
-
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) =>
@@ -40,21 +38,23 @@ export function middleware(request: NextRequest) {
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
 
+    const newURL = new URL(
+      `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
+      request.url,
+    );
+    if (search) newURL.search = search;
+    if (hash) newURL.hash = hash;
+
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url,
-      ),
-    );
+    return NextResponse.redirect(newURL);
   }
 
-  return NextResponse.next();
+  return NextResponse.next({ headers: patchHeaders(request) });
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
+  // Matcher ignoring `/_next/`, `/api/` & icons
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|apple-icon|icon).*)",
     "/api/og",
